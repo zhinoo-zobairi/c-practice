5d0018f236d61ee054c786bc54e2e100cc7d8d04
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-03 21:00:59 +0200

    OOP

---
+++ functions/OOP.c
@@ -0,0 +1,10 @@
+/*
+
+C is a procedural language, not object-oriented. That means:
+- No class keyword
+- No constructors/destructors
+- No access modifiers (public, private)
+- No member functions bound to objects
+
+*/
+
